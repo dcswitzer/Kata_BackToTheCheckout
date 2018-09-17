6542bc8ff888ee36237901899cd3f8bdcb63ca47
dcswitzer <dcswitzer@hotmail.com>
2018-09-17 12:40:56 +0200

    Initial projectsetup

---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Shouldly; 
+
+namespace Core.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void Checkout_NoItems_ShouldReturnEmptyString()
+        {
+            //Arrange
+            var checkout = new Checkout();
+            //Act
+
+            //Assert
+        }
+    }
+}
